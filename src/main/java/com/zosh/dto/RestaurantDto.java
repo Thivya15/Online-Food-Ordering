3ea498063997dfcea77eb0dc7df5e4be784e48fa
Thivya15 <thivyamahendran15@gmail.com>
2025-04-08 18:33:08 +0530

    create springboot project initially

---
+++ src/main/java/com/zosh/dto/RestaurantDto.java
@@ -0,0 +1,20 @@
+package com.zosh.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Embeddable
+public class RestaurantDto {
+    private String title;
+
+    @Column(length = 1000)  // because big string for image url.
+    private List<String> images;
+
+    private String description;
+
+    private Long id;
+}
